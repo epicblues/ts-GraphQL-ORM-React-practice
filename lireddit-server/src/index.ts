4e575be63a9c35cb66a0ae2433166f8308a7dcda
Minsung Kim <epicblue@hanmail.net>
2021-12-31 19:54:57 +0900

    Mikro-orm 기본 설정 마무리

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -8,17 +8,15 @@ import microConfig from "./mikro-orm.config";
 try {
   (async () => {
     const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
+    // await orm.getMigrator().up();
     // 다른 db 작업을 시작하기 전에 migration을 먼저 수행한다.
-    const post = orm.em.create(Post, {
-      title: "my first post",
-    });
-    // nativeinsert 메서드보다 좋은 이유: 각종 default 값을 알아서 decorator를 통해 입력해준다.
-
-    // Post 인스턴스를 생성한다. 아직까지는 DB와 관련이 없다.
-    // 사실상 new Post("my first post")와 본질적인 기능은 다르지 않다. decorator를 통한 기본값 입력 제외
+    // orm 설정에 migration 프로퍼티가 유효해야 한다.
+    const post = orm.em.create(Post, { title: "my third post" });
     await orm.em.persistAndFlush(post);
-    // db에 전송
+
+    const posts = await orm.em.find(Post, {});
+    // mongodb처럼 모든 Post 레코드 찾기
+    console.log(posts);
   })();
 
   // promise 를 return 한다.
