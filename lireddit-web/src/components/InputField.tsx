dba0cbee9addc106645defa67ab52c06fcce284a
Minsung Kim <epicblue@hanmail.net>
2022-01-03 14:52:04 +0900

    React Formik 활용 Form 인터페이스 구현, Props 설정이랑 Component 분리 참고

---
+++ lireddit-web/src/components/InputField.tsx
@@ -0,0 +1,19 @@
+import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+import { FieldHookConfig, useField } from 'formik';
+import react, { HTMLInputTypeAttribute } from 'react'
+
+type InputFieldProps = FieldHookConfig<HTMLInputTypeAttribute> & { label: string }
+
+
+
+export const InputField: React.FC<InputFieldProps> = ({ label, ...props }) => {
+  const [field, { error }] = useField(props)
+  return (
+    <FormControl isInvalid={!!error}>
+      {/* !!value => casting value to boolean */}
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} type={props.type} id={field.name} placeholder={props.placeholder} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+}
